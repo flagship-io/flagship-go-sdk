96860cd73334b60e71f6767e1ef9faeb1674135b
Guillaume <guillaume.jacquart@abtasty.com>
2022-04-08 17:12:59 +0200

    use read lock and unlock instead of write

--- pkg/bucketing/engine.go
+++ pkg/bucketing/engine.go
@@ -88,25 +88,23 @@ func (b *Engine) startTicker() {
 }
 
 func (b *Engine) getConfig() *bucketingProto.Bucketing_BucketingResponse {
-	b.configMux.Lock()
-	defer b.configMux.Unlock()
+	b.configMux.RLock()
+	defer b.configMux.RUnlock()
 	return b.config
 }
 
 // Load loads the env configuration in cache
 func (b *Engine) Load() error {
 	b.configMux.Lock()
+	defer b.configMux.Unlock()
 	newConfig, err := b.apiClient.GetConfiguration()
-	b.configMux.Unlock()
 
 	if err != nil {
 		logger.Error("Error when loading environment configuration", err)
 		return err
 	}
 
-	b.configMux.Lock()
 	b.config = newConfig
-	b.configMux.Unlock()
 
 	return nil
 }
